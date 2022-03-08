6a8244f66e8dfbfa2404c61f23074a4b37a34688
FabCom <fabcom@communs.fr>
2022-03-08 16:44:29 +0100

    use postgrsql db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -32,6 +32,9 @@ const options = {
   secret: process.env.SECRET,
   pages: {
     signIn: '/auth/signin',
-    // signOut: '/auth/signout',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   }
 };
