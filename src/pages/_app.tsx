b65cbd4684a950a7a9c163c79f1952ae9f6ce795
HugCls <superhugh92@gmail.com>
2022-03-08 20:32:34 +0100

    landing page almost done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,33 @@
-import { SessionProvider } from "next-auth/react";
+import * as React from "react";
 import { AppProps } from "next/app";
-import '../styles/global.css';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import createCache from "@emotion/cache";
+import theme from "../theme";
+import Layout from "../components/layout/Layout";
 
-const App = ({ Component, pageProps }: AppProps) => {
+export const cache = createCache({ key: "css", prepend: true });
+
+export default function MyApp(props: AppProps): JSX.Element {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
   );
-};
-
-export default App;
+}
