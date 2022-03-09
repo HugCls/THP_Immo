9b6264f6a26efe61df7b7fb487eee93a75994953
FabCom <fabcom@communs.fr>
2022-03-09 17:29:19 +0100

    custom hooks for fetch & request api

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
 import Layout from "../components/Layout"
+import useFetchGet from "../src/hooks/useFetch"
 
 const PageContact = () => {
+
+  const {isLoading, apiData: data, serverError } = useFetchGet('/api/post/getAllPosts')
+
+  console.log(data)
+
   return (
     <Layout><h1>Contact</h1></Layout>
   )
