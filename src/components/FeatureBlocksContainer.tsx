fe66a555f8203f9ac64bc620b5a1f824edcf861a
HugCls <superhugh92@gmail.com>
2022-03-10 10:37:29 +0100

    minor layout improvement

--- src/components/FeatureBlocksContainer.tsx
+++ src/components/FeatureBlocksContainer.tsx
@@ -9,7 +9,7 @@ export default function FeatureBlocksContainer({
   children,
 }: Props): JSX.Element {
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={4} sx={{mt:8}}>
       {children}
     </Grid>
   );
